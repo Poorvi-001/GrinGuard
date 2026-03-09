78d4ff937e7ec26c4d0956dd5e811a931dd9ab3b
mitta <mittalparidhi41@gmail.com>
2026-03-10 00:46:52 +0530

    cariesss

--- app/src/main/java/com/example/gringuard/User.java
+++ app/src/main/java/com/example/gringuard/User.java
@@ -2,8 +2,8 @@ package com.example.gringuard;
 
 public class User {
 
-    public String firstName;
-    public String lastName;
+    public String fName;
+    public String lName;
     public String age;
     public String gender;
     public String email;
@@ -12,11 +12,11 @@ public class User {
         // Required for Firebase
     }
 
-    public User(String firstName, String lastName,
+    public User(String fName, String lName,
                 String age, String gender, String email) {
 
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.fName = fName;
+        this.lName = lName;
         this.age = age;
         this.gender = gender;
         this.email = email;
