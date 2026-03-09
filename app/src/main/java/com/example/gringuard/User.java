473eac80c33c0251e2b87fcc39eef43c45f4a63f
Apoorva <singhapoorvahere@gmail.com>
2026-03-10 00:44:10 +0530

    changes in firebase made

--- app/src/main/java/com/example/gringuard/User.java
+++ app/src/main/java/com/example/gringuard/User.java
@@ -2,8 +2,8 @@ package com.example.gringuard;
 
 public class User {
 
-    public String firstName;
-    public String lastName;
+    public String fName;
+    public String lName;
     public String age;
     public String gender;
     public String email;
@@ -15,8 +15,8 @@ public class User {
     public User(String firstName, String lastName,
                 String age, String gender, String email) {
 
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.fName = firstName;
+        this.lName = lastName;
         this.age = age;
         this.gender = gender;
         this.email = email;
